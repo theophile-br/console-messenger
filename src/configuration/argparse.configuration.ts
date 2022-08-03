a5ec88fbd224858c56f46b02e1cd99bd0e7354ff
Théophile BRAYE <t-braye@logitud.fr>
2022-08-03 14:54:52 +0200

    fix: silent params

--- src/configuration/argparse.configuration.ts
+++ src/configuration/argparse.configuration.ts
@@ -1,3 +1,4 @@
+import { RequireArgParamsError } from "./argparse.configuration.error";
 import { Configuration } from "./configuration";
 
 export class ArgParseConfig implements Configuration {
@@ -13,10 +14,10 @@ export class ArgParseConfig implements Configuration {
     this.room = pos === -1 ? "" : process.argv[pos + 1].toUpperCase();
 
     pos = process.argv.indexOf("-s");
-    this.silent = pos !== -1;
+    this.silent = pos === -1;
 
     if (!this.pseudo) {
-      throw new Error("missing args -p {{your-pseudo}}");
+      throw new RequireArgParamsError("-p");
     }
   }
 }
